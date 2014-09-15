fdce5c814f3c72674c8a2e42628312457567a7ea
Toby Inkster <mail@tobyinkster.co.uk>
2014-09-15 22:00:17 +0100

    link to Object::Util

--- lib/Die/Hard.pm
+++ lib/Die/Hard.pm
@@ -184,13 +184,16 @@ L<http://rt.cpan.org/Dist/Display.html?Queue=Die-Hard>.
 
 L<No::Die>.
 
+The C<< $_try >> function from L<Object::Util> is a different way to
+achieve a similar effect.
+
 =head1 AUTHOR
 
 Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
 
 =head1 COPYRIGHT AND LICENCE
 
-This software is copyright (c) 2012-2013 by Toby Inkster.
+This software is copyright (c) 2012-2014 by Toby Inkster.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
